aea05064a8c4475a6b0075db3e37bc13399d1115
YES_MAIN_COM <yestang24@gmail.com>
2018-06-13 21:13:29 +0900

    버튼

--- app/src/main/java/com/example/and_real_tuto/MainActivity.kt
+++ app/src/main/java/com/example/and_real_tuto/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.and_real_tuto
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun on1(v: View){
+
+        Toast.makeText(getApplicationContext(),"Button이 눌렸습니다.",Toast.LENGTH_SHORT).show()
     }
 }
